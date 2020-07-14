f3af346524ad3b0c45bc658738307c7fd4e813ea
Adam SOudani <asoudani@email.com>
2020-07-14 10:42:45 -0700

    added gifs to read me

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # sequelizedBurger
 Sequelize Burger
-This app uses Node, Express, and Sequelize to perform queries to a dynamically created table in a database. The app is called Burger Devourer and the user will have the ability to add new burger to the uneaten list, or devour any burger of their choosing from that list. Burgers that are devoured will be moved to a parrallel list. 
+This app uses Node, Express, and Sequelize to perform queries to a dynamically created table in a database. This simple app is called Burger Devourer and the user will have the ability to add a new burger to the uneaten list, or devour any burger of their choosing from that list. Burgers that are devoured will be moved to a parrallel list. 
+
+![](burger.gif)
--- routes/burger-api-routes.js
+++ routes/burger-api-routes.js
@@ -1,41 +1,40 @@
 var db = require("../models");
 
 module.exports = function (app) {
-    app.get("/", function (req, res) {
-        db.Burgers.findAll({}).then(function (data) {
-          var result = JSON.stringify(data);
-          res.render("index", { burger: JSON.parse(result) });
-            // console.log(data);
-            // data = JSON.stringify(data)
-            // console.log(data);
-        });
+  app.get("/", function (req, res) {
+    db.Burgers.findAll({}).then(function (data) {
+      var result = JSON.stringify(data);
+      res.render("index", { burger: JSON.parse(result) });
+      // console.log(data);
+      // data = JSON.stringify(data)
+      // console.log(data);
     });
+  });
 
-    app.post("/api/burgers", function (req, res) {
-        db.Burgers.create({
-           burger_name: req.body.burger_name,
-           devoured: false
-        }).then(function (data) {
-            // res.json(data);
-            //res.json(data.toString());
-            console.log(data);
-            res.redirect('/');
-        });
+  app.post("/api/burgers", function (req, res) {
+    db.Burgers.create({
+      burger_name: req.body.burger_name,
+      devoured: false,
+    }).then(function (data) {
+      // res.json(data);
+      //res.json(data.toString());
+      console.log(data);
+      res.redirect("/");
     });
+  });
 
-    app.put("/api/burgers/:id", function (req, res) {
-        console.log(req.params.id);
-        db.Burgers.update(
-            {devoured: true},{
-            where: {
-                id: req.params.id
-            }
-            }).then(function (data) {
-            // res.json(data);
-            res.sendStatus(200);
-        });
+  app.put("/api/burgers/:id", function (req, res) {
+    console.log(req.params.id);
+    db.Burgers.update(
+      { devoured: true },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    ).then(function (data) {
+      // res.json(data);
+      res.sendStatus(200);
     });
-
-
+  });
 };
-
